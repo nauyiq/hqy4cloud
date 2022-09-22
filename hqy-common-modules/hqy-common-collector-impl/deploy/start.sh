3ff93b779ba0e69b47310754eb969e2cbf33abef
hongqy <759428167@qq.com>
2022-09-22 11:25:30 +0800

    调整启动的metaspace的内存

--- hqy-common-modules/hqy-common-collector-impl/deploy/start.sh
+++ hqy-common-modules/hqy-common-collector-impl/deploy/start.sh
@@ -17,23 +17,24 @@ export CUSTOM_SEARCH_LOCATIONS=${BASE_DIR}/init.d/,file:${BASE_DIR}/conf/
 JAVA_OPT="${JAVA_OPT} -Xmx512m -Xms256m"
 # 年轻代和年老代比例为1:2 G1垃圾回收期 如果指定NewRatio 或-Xmn参数 则G1 设定的这个目标 即MaxGCPauseMillis失效
 #JAVA_OPT="${JAVA_OPT} -XX:NewRatio=2"
+# 采用G1垃圾回收器
+JAVA_OPT="${JAVA_OPT} -XX:+UseG1GC"
 # Region size  heapSize/2048 Region size必须是2的指数 取值范围从1M到32M
 JAVA_OPT="${JAVA_OPT} -XX:G1HeapRegionSize=2m"
 JAVA_OPT="${JAVA_OPT} -XX:+ParallelRefProcEnabled"
 # 用于控制对象经过GC多少次仍然存活后晋升到老年代的最大阈值 默认15
 JAVA_OPT="${JAVA_OPT} -XX:MaxTenuringThreshold=10"
 # 元数据空间, 默认为20M
-JAVA_OPT="${JAVA_OPT} -XX:MetaspaceSize=40m"
+JAVA_OPT="${JAVA_OPT} -XX:MetaspaceSize=100m"
 # 最大元数据空间 注意：metaspace太小会引起full gc
-JAVA_OPT="${JAVA_OPT} -XX:MaxMetaspaceSize=40m"
+JAVA_OPT="${JAVA_OPT} -XX:MaxMetaspaceSize=100m"
 # 最大的可使用的直接内存
 JAVA_OPT="${JAVA_OPT} -XX:MaxDirectMemorySize=40M"
 # MaxTenuringThreshold设置垃圾的最大年龄. 默认为15 年轻代经历gc进入年老代的年龄
 JAVA_OPT="${JAVA_OPT} -XX:MaxTenuringThreshold=10"
 # 服务启动的时候真实的分配物理内存给jvm
 JAVA_OPT="${JAVA_OPT} -XX:+AlwaysPreTouch"
-# 采用G1垃圾回收器
-JAVA_OPT="${JAVA_OPT} -XX:+UseG1GC"
+
 # 当堆内存的使用率达到45%之后就会自动启动G1的并发垃圾回收 默认为45
 JAVA_OPT="${JAVA_OPT} -XX:InitiatingHeapOccupancyPercent=45"
 # 每次GC最大的停顿毫秒数
