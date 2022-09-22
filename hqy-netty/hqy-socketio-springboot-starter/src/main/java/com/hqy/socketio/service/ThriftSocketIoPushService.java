b4c466bee381ba557a15b9fda17f08acfa0a0349
hongqy <759428167@qq.com>
2022-09-22 18:32:05 +0800

    .

---
+++ hqy-netty/hqy-socketio-springboot-starter/src/main/java/com/hqy/socketio/service/ThriftSocketIoPushService.java
@@ -0,0 +1,65 @@
+package com.hqy.socketio.service;
+
+import com.facebook.swift.codec.ThriftField;
+import com.facebook.swift.service.ThriftMethod;
+import com.facebook.swift.service.ThriftService;
+import com.hqy.rpc.api.service.RPCService;
+
+import java.util.Set;
+
+/**
+ * ThriftSocketIoPushService.
+ * Socket io server must extend this interface and using ${@link ThriftService}.
+ * @author qiyuan.hong
+ * @version 1.0
+ * @date 2022/9/22 17:47
+ */
+public interface ThriftSocketIoPushService extends RPCService {
+
+    /**
+     * 单发消息-同步
+     * @param bizId         发给谁
+     * @param eventName     事件名
+     * @param wsMessageJson 事件的json数据
+     * @return 结果
+     */
+    @ThriftMethod
+    boolean syncPush(@ThriftField(1) String bizId, @ThriftField(2) String eventName, @ThriftField(3) String wsMessageJson);
+
+
+    /**
+     * 群发消息-同步
+     * @param bizIdSet      群发的用户集合
+     * @param eventName     事件名
+     * @param wsMessageJson 事件的json数据
+     * @return 结果
+     */
+    @ThriftMethod
+    boolean syncPushMultiple(@ThriftField(1) Set<String> bizIdSet, @ThriftField(2) String eventName, @ThriftField(3) String wsMessageJson);
+
+
+    /**
+     * 单发消息-异步
+     * @param bizId         发给谁
+     * @param eventName     事件名
+     * @param wsMessageJson 事件的json数据
+     * @return 结果
+     */
+    @ThriftMethod(oneway = true)
+    void asyncPush(@ThriftField(1) String bizId, @ThriftField(2) String eventName, @ThriftField(3) String wsMessageJson);
+
+
+
+    /**
+     * 群发消息-异步
+     * @param bizIdSet      群发的用户集合
+     * @param eventName     事件名
+     * @param wsMessageJson 事件的json数据
+     * @return 结果
+     */
+    @ThriftMethod(oneway = true)
+    void asyncPushMultiple(@ThriftField(1) Set<String> bizIdSet, @ThriftField(2) String eventName, @ThriftField(3) String wsMessageJson);
+
+
+
+}
---
+++ hqy-netty/hqy-socketio-springboot-starter/src/main/java/com/hqy/socketio/service/support/AbstractThriftSocketIoPushService.java
@@ -0,0 +1,63 @@
+package com.hqy.socketio.service.support;
+
+import com.hqy.ex.NettyContextHelper;
+import com.hqy.socketio.service.ThriftSocketIoPushService;
+import com.hqy.util.AssertUtil;
+import com.hqy.util.thread.ExecutorServiceProject;
+import com.hqy.util.thread.ParentExecutorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
+/**
+ * @see com.hqy.socketio.service.ThriftSocketIoPushService
+ * @author qiyuan.hong
+ * @version 1.0
+ * @date 2022/9/22 17:55
+ */
+public abstract class AbstractThriftSocketIoPushService implements ThriftSocketIoPushService {
+
+    private static final Logger log = LoggerFactory.getLogger(AbstractThriftSocketIoPushService.class);
+
+    @Override
+    public boolean syncPush(String bizId, String eventName, String wsMessageJson) {
+        try {
+            return NettyContextHelper.doPush(bizId, eventName, wsMessageJson);
+        } catch (Exception e) {
+            log.error("Failed execute to syncPush. bizId: {} | eventName: {}, wsMessageJson:{}.", bizId, eventName, wsMessageJson, e);
+            return false;
+        }
+    }
+
+    @Override
+    public boolean syncPushMultiple(Set<String> bizIdSet, String eventName, String wsMessageJson) {
+        try {
+            boolean happenError = false;
+            for (String bizId : bizIdSet) {
+                try {
+                    NettyContextHelper.doPush(bizId, eventName, wsMessageJson);
+                } catch (Throwable cause) {
+                    AssertUtil.isTrue(happenError, "syncPushMultiple consecutive errors, cause " + cause.getMessage());
+                    happenError = true;
+                    log.error("Failed execute to syncPushMultiple. bizId: {} | eventName: {}, wsMessageJson:{}.", bizId, eventName, wsMessageJson, cause);
+                }
+            }
+        } catch (Throwable cause) {
+            log.error(cause.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void asyncPush(String bizId, String eventName, String wsMessageJson) {
+        ParentExecutorService.getInstance().execute(() -> this.asyncPush(bizId, eventName, wsMessageJson), ExecutorServiceProject.PRIORITY_IMMEDIATE);
+    }
+
+    @Override
+    public void asyncPushMultiple(Set<String> bizIdSet, String eventName, String wsMessageJson) {
+        ParentExecutorService.getInstance().execute(() -> this.asyncPushMultiple(bizIdSet, eventName, wsMessageJson));
+
+    }
+}
