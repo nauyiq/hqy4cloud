2a432c892c0caedfaf51f41aa40e64f49ee7c820
hongqy <759428167@qq.com>
2022-12-30 18:12:37 +0800

    修改动态数据源模块

--- hqy-base-common-dao/hqy-base-multiple-datasource/src/main/java/com/hqy/multiple/aop/DataSourceType.java
+++ hqy-base-common-dao/hqy-base-multiple-datasource/src/main/java/com/hqy/multiple/aop/DataSourceType.java
@@ -1,5 +1,7 @@
 package com.hqy.multiple.aop;
 
+import com.hqy.multiple.MultipleDataSourceProvider;
+
 import java.lang.annotation.*;
 
 /**
@@ -12,6 +14,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface DataSourceType {
-    String value() default "default";
+
+    String value() default MultipleDataSourceProvider.DEFAULT_DATASOURCE;
 
 }
